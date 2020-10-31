fd5cc78cac130150504b0faad8b674739a844ea6
ilmira <tregulovailmira@gmail.com>
2020-10-31 21:59:00 +0200

    Fix timer

--- src/components/Watch/index.jsx
+++ src/components/Watch/index.jsx
@@ -10,7 +10,7 @@ const Watch = () => {
       isGoing &&
       setTimeout(() => {
         setTime((time) => time + 1);
-      }, 1);
+      }, 1000);
 
     return () => {
       clearTimeout(timeID);
@@ -29,16 +29,16 @@ const Watch = () => {
     setTime(0);
   };
 
-  const milliseconds = `00${~~time % 1000}`.slice(-3);
-  const seconds = `0${~~(time / 1000) % 60}`.slice(-2);
-  const minutes = `0${~~(time / (1000 * 60)) % 60}`.slice(-2);
-  const hours = `0${~~(time / (1000 * 60 * 60))}`.slice(-2);
+  const seconds = `0${~~time % 60}`.slice(-2);
+  const minutes = `0${~~(time / 60) % 60}`.slice(-2);
+  const hours = `0${~~(time / 3600)}`.slice(-2);
 
   return (
     <>
       <div className={styles.timerWrapper}>
-        <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>:
-        <span>{milliseconds}</span>
+        <span>{hours}</span>:
+        <span>{minutes}</span>:
+        <span>{seconds}</span>
       </div>
       <div className={styles.controlsWrapper}>
         <button onClick={startTimer}>Start</button>
